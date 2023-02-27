a513205b6b18ee0b4f81504a654ac2b2683d0679
Hanuš Valenta <hanus.valenta@protonmail.com>
2023-02-28 00:45:43 +0100

    I swear

--- Main.js
+++ Main.js
@@ -6,6 +6,8 @@ import * as THREE from 'three';
 const Scene = new THREE.Scene();
 const Camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const Renderer = new THREE.WebGLRenderer();
+const Light = new THREE.AmbientLight(0x404040, 20);
+const gltfLoader = new THREE.GLTFLoader();
 
 // set render size
 Renderer.setSize( window.innerWidth, window.innerHeight );
@@ -16,8 +18,12 @@ const BoxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
 const BoxMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const Cube = new THREE.Mesh( BoxGeometry, BoxMaterial );
 
-// add cube to scene
-Scene.add( Cube );
+// add cube and light to scene
+Scene.add(Light);
+Scene.add(Cube);
+
+// Load teapot
+gltfLoader.load('')
 
 // move camera a bit
 Camera.position.z = 5;
