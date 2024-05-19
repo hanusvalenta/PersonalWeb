65e382514cd3e62ec65667a3c64074ed3de78594
Hanuš Valenta <hanus.valenta@protonmail.com>
2024-05-19 23:36:43 +0200

    delete shapes js

--- Main.js
+++ Main.js
@@ -3,7 +3,55 @@ import './style.css';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-import * as Shapes from './Shapes.js';
+
+// define axies (why doesn't three have predefined variables for this?)
+const XAxis = new THREE.Vector3(1,0,0);
+const YAxis = new THREE.Vector3(0,1,0);
+const ZAxis = new THREE.Vector3(0,0,1);
+
+// heart
+const HeartShapeScale = 0.05;
+const x = 0, y = 0;
+const HeartShape = new THREE.Shape();
+
+HeartShape.moveTo( x + 5, y + 5 );
+HeartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
+HeartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
+HeartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+HeartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+HeartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
+HeartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+
+const HeartGeometry = new THREE.ShapeGeometry( HeartShape );
+const HeartMaterial = new THREE.MeshPhysicalMaterial( { color: 0xec0927 } );
+const HeartMesh = new THREE.Mesh( HeartGeometry, HeartMaterial );
+
+HeartMesh.scale.set(HeartShapeScale,HeartShapeScale,HeartShapeScale);
+
+HeartMesh.position.z = -5;
+HeartMesh.position.x = 6;
+HeartMesh.position.y = 1;
+
+HeartMesh.rotation.z = 135;
+
+// cube
+const BoxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+const BoxMaterial = new THREE.MeshPhysicalMaterial( { color: 0x000000 } );
+const Cube = new THREE.Mesh( BoxGeometry, BoxMaterial );
+
+Cube.position.z = -5;
+Cube.position.y = -10;
+Cube.position.x = 6;
+
+// tetrahedron
+
+const TetrahedronGeometry = new THREE.TetrahedronGeometry(1,0);
+const TetrahedronMaterial = new THREE.MeshPhysicalMaterial( { color: 0x000000 } );
+const Tetrahedron = new THREE.Mesh( TetrahedronGeometry, TetrahedronMaterial );
+
+Tetrahedron.position.y = -35;
+Tetrahedron.position.x = -5;
+Tetrahedron.position.z = -5;
 
 // Set constants
 const Scene = new THREE.Scene();
@@ -57,10 +105,10 @@ gltfloader.load('Suzanne.glb', function(gltf) {
 });
 
 // add objects to scene
-function LoadObjects(Light, Cube, Heart, Dodecahedron, Tetrahedron) {
-  Scene.add(Light, Cube, Heart, Dodecahedron, Tetrahedron);
+function LoadObjects(Light) {
+  Scene.add(Light, Cube, HeartMesh, Tetrahedron);
 }
-LoadObjects(Light, Shapes.Cube, Shapes.HeartMesh, Shapes.Dodecahedron, Shapes.Tetrahedron);
+LoadObjects(Light);
 
 // scroll animation
 function MoveCamera() {
@@ -79,14 +127,11 @@ function Animate() {
   Renderer.render(Scene, Camera);
 
   // rotate objects
-  Shapes.Cube.rotation.y += 0.001;
-  Shapes.Cube.rotation.x += 0.001;
+  Cube.rotation.y += 0.001;
+  Cube.rotation.x += 0.001;
 
-  Shapes.Dodecahedron.rotation.y += 0.001;
-  Shapes.Dodecahedron.rotation.x += 0.001;
-
-  Shapes.Tetrahedron.rotation.y += 0.001;
-  Shapes.Tetrahedron.rotation.x += 0.001;
+  Tetrahedron.rotation.y += 0.001;
+  Tetrahedron.rotation.x += 0.001;
 }
 
 Animate();
